48a4b01049ef44b74e72f7ed85498f438b424975
sergio semedi <ssemedi@opennebula.systems>
2018-06-02 15:42:27 +0200

    test petition finished

--- API/controller.js
+++ API/controller.js
@@ -5,6 +5,7 @@ Task = mongoose.model('airport');
 var Amadeus = require('amadeus');
 var amadeus = new Amadeus({clientId: process.env.AMADEUS_CLIENT_ID, clientSecret: process.env.AMADEUS_CLIENT_SECRET });
 
+
 exports.list_all_airports = function(req, res) {
     Task.find({}, function(err, task) {
         if (err)
@@ -16,12 +17,21 @@ exports.list_all_airports = function(req, res) {
 exports.test = function(req, res) {
     //
     //amadeus
-    var api_result = amadeus.shopping.flightDestination.get({
+    var api_result = amadeus.shopping.flightDestinations.get({
         origin: 'MAD',
         maxPrice: 200
+    })
+
+    api_result.then(function(response){
+        res.json(response.result)
+    }).catch(function(error){
+        console.log(error.response); //=> The response object with (un)parsed data
+        console.log(error.response.request); //=> The details of the request made
+        console.log(error.code); //=> A unique error code to identify the type of error
     });
 
-    res.json(api_result);
+
+    //res.json(api_result);
 
 };
 
